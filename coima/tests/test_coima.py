eb0598af83703fc7319f1965f1c222b120692e43
Alfredo Deza <alfredodeza@gmail.com>
2010-12-04 14:47:50 -0500

    removed cruft

--- coima/tests/test_coima.py
+++ coima/tests/test_coima.py
@@ -1,6 +1,5 @@
 from mock import MockSys
 import sys
-from StringIO import StringIO
 from unittest import TestCase
 from coima import Template
 
@@ -10,11 +9,9 @@ class TestCoima(TestCase):
     def test_simple_render(self):
         replace = {'port':'8080', 'host':'localhost'}
         t = Template(replace, 'foo.conf')
-        #out = StringIO()
         sys.stdout = MockSys()
         t.render()
         result = sys.stdout.captured()
-        #result = sys.stdout.captured()
         expected = '# Simple Configuration File for Testing\n#\nport = 8080\nhost = localhost\n\n\n\n'
         self.assertEqual(result, expected)
 
